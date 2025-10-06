f06db2d9196be5fdec9aec22a3969c28d9ab8b63
Dongwon <Dongwon.Shin@gmail.com>
2025-10-06 17:06:08 -0400

    ML pane - ready to wire Accuaracy_A

--- ascends_server.py
+++ ascends_server.py
@@ -16,7 +16,7 @@ matplotlib.use("Agg")  # headless, no GUI windows
 import matplotlib.pyplot as plt
 import dcor
 from sklearn.feature_selection import mutual_info_regression, mutual_info_classif
-from fastapi import FastAPI, File, Form, UploadFile
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
